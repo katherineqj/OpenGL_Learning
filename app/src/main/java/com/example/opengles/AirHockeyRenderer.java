ee7caedcbbcb9284df24188c3e283800de2bc996
katherineqj <156860180@qq.com>
2019-09-17 15:55:18 +0800

    课后练习

--- app/src/main/java/com/example/opengles/AirHockeyRenderer.java
+++ app/src/main/java/com/example/opengles/AirHockeyRenderer.java
@@ -40,16 +40,19 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         mContext = context;
 
         float[] tableVerticesWithTriangles = {
-
                 0f, 0f, 1f, 1f, 1f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 1f, 0f, 0f,
+                0f, -0.5f, 0f, 1f, 0f,
+                0.5f, -0.5f, 0f, 0f, 0.1f,
+                0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0.5f, 0f, 1f, 0f,
+                0f, 0.5f,  0f, 0f, 0.1f,
+                -0.5f, 0.5f,1f, 0f, 0f,
+                -0.5f, 0f,  0f, 1f, 0f,
+                -0.5f, -0.5f, 0f, 0f, 0.1f,
 
                 -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 0f, 1f, 0f,
 
                 0f, -0.25f, 0f, 0f, 1f,
                 0f, 0.25f, 1f, 0f, 0f
@@ -101,18 +104,13 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         //关联属性与顶点数据的数组
         GLES20.glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData);
         GLES20.glEnableVertexAttribArray(aColorLocation);
-
     }
-
     @Override
     public void onSurfaceChanged(GL10 gl10, int i, int i1) {
         // 在Surface被创建以后，每次Surface尺寸发生变化时，这个方法都会被GLSurfaceView调用到，
         // 比如横竖屏切换，尺寸就会发生变化
         GLES20.glViewport(0, 0, i, i1); //设置视口尺寸，告诉opengl可以用来渲染的surface大小
-
-
     }
-
     @Override
     public void onDrawFrame(GL10 gl10) {
         // 当绘制一帧时，这个方法会被GLSurfaceView调用，在这个方法中，我们一定要绘制一些东西，
@@ -120,13 +118,13 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);//清空屏幕，并且用之前 glClearColor定义的颜色填充整个屏幕
 
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 10);
 
-        GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
+        GLES20.glDrawArrays(GLES20.GL_LINES, 10, 2);
 
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 12, 1);
 
-        GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 13, 1);
 
 
     }
